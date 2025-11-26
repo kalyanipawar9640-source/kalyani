b9cf2b84b2856afbc07ea37b95f9527fd69b45c7
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-11-26 23:24:25 +0530

    Add files via upload

---
+++ java2/PrimitiveTypes.java
@@ -0,0 +1,23 @@
+public class PrimitiveTypes{
+    public static void main(String[]args) {
+        //Primitive data Types in java
+        byte b=100;
+        short S =30000;
+        int i = 100000;
+        long l = 10000000000L;
+        float f = 3.14f;
+        double d = 99.99;
+        char c = 'A';
+        boolean bool = true;
+
+        //Printing all primitive types
+        System.out.println("byte value:"+b);
+        System.out.println("short value:"+S);
+        System.out.println("int value:"+i);
+        System.out.println("long value:"+l);
+        System.out.println("float value:"+f);
+        System.out.println("double value:"+d);
+        System.out.println("char value:"+c);
+        System.out.println("boolean value:"+bool);
+    }
+}
