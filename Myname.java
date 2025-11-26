02901a01d0a4a6eba90516475a035b7de2479316
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-11-26 22:08:56 +0530

    Delete Myname.java

--- Myname.java
+++
@@ -1,6 +0,0 @@
-public class Myname {
-    public static void main(String[] args) {
-        System.out.println("Name: Kalyani Pawar");
-        System.out.println("Enrollment Number: 0873CS241081");
-    }
-}
