4b6d4f7c9651bec59fd60678c6a0c20dfd3198af
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-11-26 23:07:39 +0530

    Add files via upload

---
+++ java2/Asciivalue.java
@@ -0,0 +1,9 @@
+public class Asciivalue {
+    public static void main(String []args) {
+        char ch = 'K';
+        int Ascii =(int) ch;
+
+        System.out.println("character:"+ch);
+        System.out.println("Ascii value:"+Ascii);
+    }
+}
