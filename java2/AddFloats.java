5b61cc8a9db0b06bd812fe02e67185c7526749e0
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-11-26 23:00:16 +0530

    Add files via upload

---
+++ java2/AddFloats.java
@@ -0,0 +1,14 @@
+public class AddFloats {
+    public static void main (String[]args) {
+        Float num1=12.5f;
+        Float num2=7.3f;
+
+        //Add two numbers
+        Float sum = num1 + num2;
+
+        //print result
+        System.out.println("First number:"+num1);
+        System.out.println("Second number:"+num2);
+        System.out.println("sum:"+sum);
+    }   
+}
