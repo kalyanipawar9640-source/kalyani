0d7ae97f27ab3e753c2adbe5412be46e5f772cfd
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-11-26 23:37:10 +0530

    Add files via upload

---
+++ java2/TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting {
+    public static void main(String[]args) {
+        double numDouble = 9.78;
+
+        //Type casting from double to int
+        int numInt = (int)numDouble;
+
+        System.out.println("Double Value:"+numDouble);
+        System.out.println("After Casting to int:"+numInt);
+    }
+}
