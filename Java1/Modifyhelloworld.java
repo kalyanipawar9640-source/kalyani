39013159db803820052b729916e745c8c33e8ea9
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-11-26 22:40:13 +0530

    Add files via upload

---
+++ Java1/Modifyhelloworld.java
@@ -0,0 +1,8 @@
+public class Modifyhelloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("java language");
+        System.out.println("java is a programming language");
+    }
+}
+
