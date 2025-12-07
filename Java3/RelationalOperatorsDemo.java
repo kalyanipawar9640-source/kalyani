08764ac31f21d736f1979a504d5ff5a999405dcd
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-12-07 15:42:32 +0530

    Add files via upload

---
+++ Java3/RelationalOperatorsDemo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RelationalOperatorsDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("\n--- Relational Operator Results ---");
+        System.out.println(a + " == " + b + " : " + (a == b));
+        System.out.println(a + " != " + b + " : " + (a != b));
+        System.out.println(a + " > "  + b + " : " + (a > b));
+        System.out.println(a + " < "  + b + " : " + (a < b));
+        System.out.println(a + " >= " + b + " : " + (a >= b));
+        System.out.println(a + " <= " + b + " : " + (a <= b));
+    }
+}
+
