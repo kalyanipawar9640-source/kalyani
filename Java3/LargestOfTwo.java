69be3605e1365935b643d520b37e4db4dd53fcab
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-12-07 15:40:57 +0530

    Add files via upload

---
+++ Java3/LargestOfTwo.java
@@ -0,0 +1,11 @@
+public class LargestOfTwo {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+
+        int largest = (a > b) ? a : b;
+
+        System.out.println("The largest number is: " + largest);
+    }
+}
+
