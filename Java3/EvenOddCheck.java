6957740614abc1a0e439ad55f3bc0725e150a72b
kalyanipawar9640-source <kalyanipawar9640@gmail.com>
2025-12-07 15:40:04 +0530

    Add files via upload

---
+++ Java3/EvenOddCheck.java
@@ -0,0 +1,11 @@
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int number = 10; // You can change this value to test other numbers
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
